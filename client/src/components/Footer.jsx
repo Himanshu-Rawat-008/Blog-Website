aa55dc99da38d52d26676ab80628f92180f05073
Himanshu Rawat <himanshurawat008.hr@gmail.com>
2025-03-21 19:33:29 +0530

    Dark Mode

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,27 +4,34 @@ import { BsFacebook, BsGithub, BsInstagram, BsTwitterX } from 'react-icons/bs';
 
 export default function FooterComponent() {
     return (
-        <Footer container className='w-full border border-t-8 border-teal-500'>
-            <div className='w-full mx-auto'>
+        <Footer container className='border border-t-8 border-teal-500'>
+            <div className='w-full max-w-7xl mx-auto'>
                 <div className='grid w-full justify-between sm:flex md:grid-cols-1'>
                     <div className='mt-5'>
-                        <Link to="/" className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'>
-                            <span className='px-2 py-1 bg-gradient-to-r from-cyan-500 via-purple-500 to-pink-500 rounded-lg text-white'>WHAT</span>
+                        <Link 
+                            to="/" 
+                            className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'>
+                            <span 
+                                className='px-2 py-1 bg-gradient-to-r from-cyan-500 via-purple-500 to-pink-500 rounded-lg text-white'>WHAT</span>
                                         Blog
                         </Link>
                     </div>
-                    <div className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
+                    <div 
+                        className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
                         <div>
-                            <Footer.Title title='About' />
+                            <Footer.Title 
+                                title='About' />
                             <Footer.LinkGroup col>
                                 <Footer.Link 
-                                    href="" target='_blank' 
+                                    href="" 
+                                    target='_blank' 
                                     rel='noopener noreferrer'
                                 >
                                     My Profile
                                 </Footer.Link>
                                 <Footer.Link 
-                                    href="" target='_blank' 
+                                    href="" 
+                                    target='_blank' 
                                     rel='noopener noreferrer'
                                 >
                                     Github
@@ -32,16 +39,19 @@ export default function FooterComponent() {
                             </Footer.LinkGroup>
                         </div>
                         <div>
-                            <Footer.Title title='Follow Us' />
+                            <Footer.Title 
+                                title='Follow Us' />
                             <Footer.LinkGroup col>
                                 <Footer.Link 
-                                    href="" target='_blank' 
+                                    href=""
+                                    target='_blank' 
                                     rel='noopener noreferrer'
                                 >
                                     My LinkedIn
                                 </Footer.Link>
                                 <Footer.Link 
-                                    href="" target='_blank' 
+                                    href="" 
+                                    target='_blank' 
                                     rel='noopener noreferrer'
                                 >
                                     Discord
@@ -49,16 +59,19 @@ export default function FooterComponent() {
                             </Footer.LinkGroup>
                         </div>
                         <div>
-                            <Footer.Title title='Legal' />
+                            <Footer.Title 
+                                title='Legal' />
                             <Footer.LinkGroup col>
                                 <Footer.Link 
-                                    href="" target='_blank' 
+                                    href=""
+                                    target='_blank' 
                                     rel='noopener noreferrer'
                                 >
                                     Privacy Policy
                                 </Footer.Link>
                                 <Footer.Link 
-                                    href="" target='_blank' 
+                                    href=""
+                                    target='_blank' 
                                     rel='noopener noreferrer'
                                 >
                                     Terms &amp; Conditions
@@ -69,12 +82,23 @@ export default function FooterComponent() {
                 </div>
                 <Footer.Divider />
                 <div className='w-full sm:flex sm:items-center sm:justify-between'>
-                    <Footer.Copyright href='#' by="Himanshu Rawat" year={new Date().getFullYear()} />
+                    <Footer.Copyright
+                        href='#'
+                        by="Himanshu Rawat"
+                        year={new Date().getFullYear()} />
                     <div className='flex gap-6 mt-4 sm:mt-0  sm:justify-center'>
-                        <Footer.Icon href="#" icon={BsFacebook}/>
-                        <Footer.Icon href="#" icon={BsInstagram}/>
-                        <Footer.Icon href="#" icon={BsGithub}/>
-                        <Footer.Icon href="#" icon={BsTwitterX}/>
+                        <Footer.Icon 
+                            href="#"
+                            icon={BsFacebook}/>
+                        <Footer.Icon
+                            href="#"
+                            icon={BsInstagram}/>
+                        <Footer.Icon 
+                            href="#"
+                            icon={BsGithub}/>
+                        <Footer.Icon 
+                            href="#" 
+                            icon={BsTwitterX}/>
                     </div>
                 </div>
             </div>
---
+++ client/src/components/ThemeProvider.jsx
@@ -0,0 +1,18 @@
+import { useSelector } from 'react-redux';
+import React from 'react';
+
+export default function ThemeProvider({ children }) {
+    const {theme} = useSelector(state => state.theme);
+    return (
+        <div className={theme}>
+            <div className='bg-white
+            text-gray-700
+            dark:text-gray-200 
+            min-h-screen
+            dark:bg-[rgb(16,23,42)] '
+            >
+                {children}
+            </div>
+        </div>
+    );
+}
